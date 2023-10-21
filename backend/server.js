61f6450244ce7633d6720321c42481be66ec1862
julio cesar <jcmcf2012@gmail.com>
2023-10-21 14:01:35 -0300

    infra: first setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import { WebSocket } from "ws";
+
+// const serverAddress = "ws://127.0.0.1:5000";
+const serverAddress = "wss://puc-websocket-project.glitch.me/";
+
+const ws = new WebSocket(serverAddress, {
+  headers: {
+    "user-agent": "Mozilla",
+  },
+});
+
+ws.on("open", function () {
+  ws.send("Hello from PCamp!");
+});
+
+ws.on("message", function (msg) {
+  console.log("Received msg from the server: " + msg);
+});
---
+++ backend/test.js
@@ -0,0 +1,15 @@
+const WebSocket = require("ws");
+const PORT = 5000;
+
+const wsServer = new WebSocket.Server({
+  port: PORT,
+});
+
+wsServer.on("connection", function (socket) {
+  console.log("Client connected");
+
+  socket.on("message", function (msg) {
+    console.log("Received message" + msg);
+  });
+  //wsServer.send("Hello you have been conected!");
+});
